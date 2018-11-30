1cac7bda11769b8d2001dcf4170b0d6b86661a1b
sochooligan <sochooligan@163.com>
2018-11-30 15:14:36 +0800

    抽象工厂

--- MyLexi/code/gui/GUIFactory.h
+++ MyLexi/code/gui/GUIFactory.h
@@ -5,6 +5,21 @@
 /*
   2018年11月30日 星期五 11时32分07秒 CST
 */
+
+/*
+  An application that runs on more than one platform must conform to
+  the user interface style guide on each platform.
+
+  This class is an abstract class that defines interface for creating
+  widget glyphs.
+
+  The guiFactory object
+
+      abstracts the process of creating scroll bars for any
+      look-and-feel standard.  It can manufacture a full range of
+      widget glyphs, including scrollbars, buttons, entry fields,
+      menus, and so forth.
+*/
 class GUIFactory {
 public:
     virtual void CreateScrollBar();
